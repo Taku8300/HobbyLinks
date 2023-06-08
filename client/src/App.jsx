7dc898c05d1e67b65d7edd4c257d0308da7bcf4f
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-09 05:59:40 +0900

    add Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,26 @@ import { useEffect, useState } from "react";
 import Listview from "./components/Listview";
 import Searchbar from "./components/Searchbar";
 import Map from "./components/Map";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [showList, setShowList] = useState("group");
+
+  useEffect(() => {
+    console.log(showList);
+  }, [showList]);
+
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <div className="max-w-7xl mx-auto grid grid-cols-12  gap-5 ">
-        <div className="col-span-2 bg-red-200">1</div>
-        <div className="col-span-8 ">
-          <Searchbar></Searchbar>
-          <Listview></Listview>
+    <div className='bg-neutral-200 min-h-screen '>
+      <div className='max-w-7xl mx-auto grid grid-cols-12  '>
+        <div className='col-span-2'>
+          <Sidebar></Sidebar>
         </div>
-        <div className="col-span-2">
+        <div className='col-span-8 '>
+          <Searchbar setShowList={setShowList} showList={showList}></Searchbar>
+          <Listview showList={showList}></Listview>
+        </div>
+        <div className='col-span-2'>
           <Map></Map>
         </div>
       </div>
--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,50 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Filter from "./Filter";
 
-function Searchbar() {
+function Searchbar({ showList, setShowList }) {
+  const handleEventbtn = () => {
+    setShowList("event");
+  };
+  const handleGroupbtn = () => {
+    setShowList("group");
+  };
+
   return (
-    <div className="max-w-4xl mx-auto ">
-      <div className="flex justify-start my-4 mx-auto">
+    <div className='max-w-4xl mx-auto sticky top-0 z-10 backdrop-blur-lg border-b pt-1 '>
+      <div className='flex justify-start my-2 mx-auto px-2'>
         <input
-          type="text"
-          placeholder="Search..."
-          className="py-2 px-4 border border-gray-300 rounded-l-md focus:outline-none  focus:border-transparent w-60 "
+          type='text'
+          placeholder='Search...'
+          className='py-2 px-4 border border-gray-300 rounded-l-md focus:outline-none'
         />
         <input
-          type="text"
-          value="Osaka, JP"
-          className="py-2 px-4 border border-gray-300 focus:outline-none  focus:border-transparent w-60"
+          type='text'
+          value='Osaka, JP'
+          className='py-2 px-4 border border-gray-300 focus:outline-none w-40'
         />
-        <button className="bg-purple-500 text-white py-2 px-4 rounded-r-md hover:bg-purple-600">
-          <i className="fas fa-search"></i>
+        <button className='bg-purple-500 text-white py-2 px-4 rounded-r-md hover:bg-purple-600'>
+          <i className='fas fa-search'></i>
         </button>
       </div>
-      <div className="flex justify-start gap-1 mx-auto">
-        <select
-          name="Day"
-          className="py-2 px-4  border border-gray-300 rounded-sm focus:outline-none  focus:border-transparent"
+
+      <div className='flex gap-0.5 '>
+        <button
+          className={`w-1/2 h-fit max-w-md mt-2 mb-1 text-2xl text-neutral-800  font-bold transition duration-150 border-b-8 ${
+            showList === "group" ? "border-purple-500" : "border-transparent"
+          } hover:bg-neutral-300`}
+          onClick={() => handleGroupbtn()}
         >
-          <option value="">Day</option>
-        </select>
-        <select
-          name="Type"
-          className="py-2 px-4  border border-gray-300 rounded-sm focus:outline-none  focus:border-transparent"
+          Groups
+        </button>
+        <button
+          className={`w-1/2 h-fit  max-w-md mt-2 text-2xl text-neutral-800  font-bold transition duration-150 border-b-8 ${
+            showList === "event" ? "border-purple-500" : "border-transparent"
+          } hover:bg-neutral-300`}
+          onClick={() => handleEventbtn()}
         >
-          <option value="">Type</option>
-        </select>
-        <select
-          name="Distance"
-          className="py-2 px-4  border border-gray-300 rounded-sm focus:outline-none  focus:border-transparent"
-        >
-          <option value="">Distance</option>
-        </select>
-        <select
-          name="Category"
-          className="py-2 px-4  border border-gray-300 rounded-sm focus:outline-none  focus:border-transparent"
-        >
-          <option value="">Category</option>
-        </select>
-        <button className="bg-purple-500 text-white py-2 px-4 rounded-md hover:bg-purple-600">
-          Filter
+          Events
         </button>
       </div>
     </div>
