712dc8ab7e1f20f06a36e98999a0ba30c90cb501
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-16 09:40:30 +0900

    Event Page Styling

--- client/src/components/eventComponents/EventDesc.jsx
+++ client/src/components/eventComponents/EventDesc.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function EventDesc({ desc }) {
   return (
     <div>
-      <h1>{Date.now().toLocaleString}</h1>
+      <h1>{new Date().toISOString()}</h1>
       <h2>3 Chome-中２−１ Chiyozaki, Nishi Ward, Osaka, 550-0023</h2>
       <div>Atendee list</div>
       <div>{desc}</div>
