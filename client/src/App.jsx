aadd75d20677890e81bf937bd176604e7505ae4d
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-21 22:50:33 +0900

    move header to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainPage from "./pages/Mainpage";
+import MainPage from "./pages/MainPage";
 import GroupPage from "./pages/GroupPage";
 import EventPage from "./pages/EventPage";
-
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/group/:groupId" element={<GroupPage />} />
-        <Route path="/event/:eventId" element={<EventPage />} />
+        <Route path='/' element={<MainPage />} />
+        <Route path='/group/:groupId' element={<GroupPage />} />
+        <Route path='/event/:eventId' element={<EventPage />} />
       </Routes>
     </BrowserRouter>
   );
