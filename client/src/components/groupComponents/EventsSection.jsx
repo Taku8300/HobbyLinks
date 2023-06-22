15731c522a703af55f23a2d357a4f8f41f77dcb9
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-23 07:22:48 +0900

    update styling

--- client/src/components/groupComponents/EventsSection.jsx
+++ client/src/components/groupComponents/EventsSection.jsx
@@ -2,42 +2,39 @@ import React from "react";
 
 function EventsSection({ events }) {
   return (
-    <div className=" my-4 mx-auto w-full ">
-      <h1 className="text-3xl font-bold">Upcoming Events</h1>
+    <div className=' my-4  w-full  px-5'>
+      <h1 className='text-3xl font-bold'>Upcoming Events</h1>
       {/* query all the event from this group */}
-      <div className="bg-slate-100 mt-4">
-        <div className="flex">
-          <img src="" alt="" className="w-52" />
+      <div className='bg-slate-100 mt-4'>
+        <div className='flex'>
+          <img src='' alt='' className='w-52' />
           <div>
-            <h1 className="text-xl">Event_Name</h1>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repudiandae, eveniet fugiat saepe esse error accusantium ipsa
-            praesentium est quos ipsum voluptates! Tempore vel velit incidunt
-            voluptatem in accusantium blanditiis voluptates.
+            <h1 className='text-xl'>Event_Name</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, eveniet fugiat
+            saepe esse error accusantium ipsa praesentium est quos ipsum voluptates! Tempore vel
+            velit incidunt voluptatem in accusantium blanditiis voluptates.
           </div>
         </div>
       </div>
-      <div className=" bg-slate-100 mt-4">
-        <div className="flex">
-          <img src="" alt="" className="w-52" />
+      <div className=' bg-slate-100 mt-4'>
+        <div className='flex'>
+          <img src='' alt='' className='w-52' />
           <div>
-            <h1 className="text-xl">Event_Name</h1>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repudiandae, eveniet fugiat saepe esse error accusantium ipsa
-            praesentium est quos ipsum voluptates! Tempore vel velit incidunt
-            voluptatem in accusantium blanditiis voluptates.
+            <h1 className='text-xl'>Event_Name</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, eveniet fugiat
+            saepe esse error accusantium ipsa praesentium est quos ipsum voluptates! Tempore vel
+            velit incidunt voluptatem in accusantium blanditiis voluptates.
           </div>
         </div>
       </div>
-      <div className=" bg-slate-100 mt-4">
-        <div className="flex">
-          <img src="" alt="" className="w-52" />
+      <div className=' bg-slate-100 mt-4'>
+        <div className='flex'>
+          <img src='' alt='' className='w-52' />
           <div>
-            <h1 className="text-xl">Event_Name</h1>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repudiandae, eveniet fugiat saepe esse error accusantium ipsa
-            praesentium est quos ipsum voluptates! Tempore vel velit incidunt
-            voluptatem in accusantium blanditiis voluptates.
+            <h1 className='text-xl'>Event_Name</h1>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, eveniet fugiat
+            saepe esse error accusantium ipsa praesentium est quos ipsum voluptates! Tempore vel
+            velit incidunt voluptatem in accusantium blanditiis voluptates.
           </div>
         </div>
       </div>
--- client/src/pages/GroupPage.jsx
+++ client/src/pages/GroupPage.jsx
@@ -22,9 +22,7 @@ function GroupPage() {
   useEffect(() => {
     const fetchGroupData = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:8000/api/groups/${groupId}`
-        );
+        const response = await axios.get(`http://localhost:8000/api/groups/${groupId}`);
         console.log(response);
         setGroupDetails({
           title: response.data.group_name,
@@ -41,58 +39,52 @@ function GroupPage() {
   }, [groupId]);
   return (
     <>
-      <div className="min-h-screen bg-slate-50">
-        <div className="flex flex-col mb-2 mx-auto max-w-5xl bg-white px-5 py-5 shadow-lg min-h-screen">
+      <div className='min-h-screen bg-slate-50'>
+        <div className='flex flex-col mb-2 mx-auto max-w-5xl bg-white px-5 py-5 shadow-lg min-h-screen'>
           {/* btnsection */}
-          <GroupHeader
-            groupId={groupId}
-            imgUrl={groupDetails.imgUrl}
-            title={groupDetails.title}
-          />
+          <GroupHeader groupId={groupId} imgUrl={groupDetails.imgUrl} title={groupDetails.title} />
 
-          <div className="flex">
+          <div className='flex '>
             <div>
               <button
-                type=""
-                className="w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2 ml-5"
+                type=''
+                className='w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2 ml-5'
               >
                 About
               </button>
               <button
-                type=""
-                className="w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2"
+                type=''
+                className='w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2'
               >
                 Members
               </button>
               <button
-                type=""
-                className="w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 
-                text-center m-2"
+                type=''
+                className='w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 
+                text-center m-2'
               >
                 Events
               </button>
               <button
-                type=""
-                className="w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2"
+                type=''
+                className='w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2'
               >
                 Picture
               </button>
               <button
-                type=""
-                className="w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2"
+                type=''
+                className='w-[130px] border hover:border-purple-500 border-b-4 font-medium rounded-lg text-lg px-5 py-2.5 text-center m-2'
               >
                 Discussion
               </button>
             </div>
             <div>
-              <button
-                type=""
-                className="w-30 text-white bg-purple-500 hover:bg-purple-800 hover:border-purple-600 border-purple-400 border-b-4   rounded-lg text-xl font-bold px-5 py-2.5 text-center m-2 ml-12"
-              >
+              <button className='w-30 text-white bg-purple-500 hover:bg-purple-800 hover:border-purple-600 border-purple-400 border-b-4   rounded-lg  font-bold px-5 py-2.5 text-center m-2 ml-10'>
                 Join This Group
               </button>
             </div>
           </div>
+
           <About desc={groupDetails.desc} />
           <EventsSection events={groupDetails.events} />
           <Photo />
