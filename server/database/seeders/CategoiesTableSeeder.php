3562f22c6676e1525d2edf8fbba1a82de1c5c35d
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-06-26 09:11:03 +0900

    fix categorySeeder

--- server/database/seeders/CategoiesTableSeeder.php
+++ server/database/seeders/CategoiesTableSeeder.php
@@ -16,18 +16,10 @@ class CategoiesTableSeeder extends Seeder
     {
         //
         DB::transaction(function () {
+
             Category::insert([
                 [
                     'category_id' => '1',
-                    'category_name' => 'bike',
-                    'created_at' => '2023/06/19',
-                    'updated_at' => '2023/06/19',
-                    'deleted_at' => '2023/06/19',
-                ]
-            ]);
-            Category::insert([
-                [
-                    'category_id' => '2',
                     'category_name' => 'Games',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -36,7 +28,7 @@ class CategoiesTableSeeder extends Seeder
             ]);
             Category::insert([
                 [
-                    'category_id' => '3',
+                    'category_id' => '2',
                     'category_name' => 'Sports',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -46,7 +38,7 @@ class CategoiesTableSeeder extends Seeder
 
             Category::insert([
                 [
-                    'category_id' => '4',
+                    'category_id' => '3',
                     'category_name' => 'Music',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -55,7 +47,7 @@ class CategoiesTableSeeder extends Seeder
             ]);
             Category::insert([
                 [
-                    'category_id' => '5',
+                    'category_id' => '4',
                     'category_name' => 'Pets & Animals',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -64,7 +56,7 @@ class CategoiesTableSeeder extends Seeder
             ]);
             Category::insert([
                 [
-                    'category_id' => '6',
+                    'category_id' => '5',
                     'category_name' => 'Social Activites',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -73,7 +65,7 @@ class CategoiesTableSeeder extends Seeder
             ]);
             Category::insert([
                 [
-                    'category_id' => '7',
+                    'category_id' => '6',
                     'category_name' => 'Travel & outdoor',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -83,7 +75,7 @@ class CategoiesTableSeeder extends Seeder
 
             Category::insert([
                 [
-                    'category_id' => '8',
+                    'category_id' => '7',
                     'category_name' => 'Techology',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
@@ -92,7 +84,7 @@ class CategoiesTableSeeder extends Seeder
             ]);
             Category::insert([
                 [
-                    'category_id' => '9',
+                    'category_id' => '8',
                     'category_name' => 'Art & Culture
                     ',
                     'created_at' => '2023/06/19',
@@ -102,7 +94,7 @@ class CategoiesTableSeeder extends Seeder
             ]);
             Category::insert([
                 [
-                    'category_id' => '10',
+                    'category_id' => '9',
                     'category_name' => 'Career & buisness',
                     'created_at' => '2023/06/19',
                     'updated_at' => '2023/06/19',
